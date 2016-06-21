af3c5a251ee61101f2823d1d5dc8a35ab614bd3f
Tej Chajed <tchajed@mit.edu>
2016-06-20 18:18:27 -0700

    Implement one branch of subst
    Assuming we have the type of subst correct

--- system-t.v
+++ system-t.v
@@ -1,6 +1,8 @@
 Require List.
 Import List.ListNotations.
 Open Scope list.
+Require Import Equality.
+Import EqdepTheory.
 
 Set Implicit Arguments.
 
@@ -26,6 +28,35 @@ Definition variable_add Gamma t (v: variable Gamma t) t' :
   variable (t' :: Gamma) t :=
   var_later t' v.
 
+Fixpoint var_index Gamma t (v: variable Gamma t) : nat :=
+  match v with
+  | var_here _ _ => 0
+  | var_later _ v => S (var_index v)
+  end.
+
+Definition var_index_eq Gamma t1 t2 (v1: variable Gamma t1) (v2: variable Gamma t2) :
+  {var_index v1 = var_index v2} + {var_index v1 <> var_index v2}.
+Proof.
+  decide equality.
+Qed.
+
+Definition var_indices_eq : forall Gamma t1 t2 (v1: variable Gamma t1) (v2: variable Gamma t2),
+    var_index v1 = var_index v2 ->
+    { H : t1 = t2 & eq_rect _ (fun t => variable Gamma t) v1 _ H = v2 }.
+Proof.
+  intros.
+  dependent induction Gamma.
+  inversion v1.
+  dependent induction v1;
+    dependent induction v2;
+    simpl in *;
+    try congruence.
+  exists eq_refl; cbn; auto.
+  inversion H.
+  destruct (IHGamma _ _ _ _ H1); subst.
+  exists eq_refl; cbn; auto.
+Defined.
+
 Inductive hasTy (Gamma: mapping) : type -> Type :=
 | var : forall t (v: variable Gamma t), hasTy Gamma t
 | zero : hasTy Gamma natTy
@@ -43,3 +74,16 @@ Inductive hasTy (Gamma: mapping) : type -> Type :=
       (e: hasTy (t :: Gamma) t)
       (n: hasTy Gamma natTy),
       hasTy Gamma t.
+
+Definition subst Gamma t (x: variable Gamma t) (e': hasTy Gamma t) t' (e: hasTy Gamma t') : hasTy Gamma t'.
+  intros.
+  remember e.
+  induction e.
+  destruct (var_index_eq v x).
+  abstract (apply var_indices_eq in e;
+    destruct e; subst;
+    exact e').
+  exact (var v).
+
+  all: exact h.
+Defined.
