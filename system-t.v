65ea2e65ff9310a4822c184e3bb8cd5eb37560b7
Daniel Ziegler <dmz@mit.edu>
2016-06-21 12:30:16 -0700

    s/hasTy/expr/g

--- system-t.v
+++ system-t.v
@@ -57,57 +57,57 @@ Proof.
   exists eq_refl; cbn; auto.
 Defined.
 
-Inductive hasTy (Gamma: mapping) : type -> Type :=
-| var : forall t (v: variable Gamma t), hasTy Gamma t
-| zero : hasTy Gamma natTy
-| succ : hasTy Gamma natTy -> hasTy Gamma natTy
+Inductive expr (Gamma: mapping) : type -> Type :=
+| var : forall t (v: variable Gamma t), expr Gamma t
+| zero : expr Gamma natTy
+| succ : expr Gamma natTy -> expr Gamma natTy
 | abs : forall t1 t2,
-    forall (e: hasTy (t1 :: Gamma) t2),
-      hasTy Gamma (arrow t1 t2)
+    forall (e: expr (t1 :: Gamma) t2),
+      expr Gamma (arrow t1 t2)
 | app : forall t1 t2,
-    forall (e1: hasTy Gamma (arrow t1 t2))
-      (e2: hasTy Gamma t1),
-      hasTy Gamma t2
+    forall (e1: expr Gamma (arrow t1 t2))
+      (e2: expr Gamma t1),
+      expr Gamma t2
 | iter : forall t,
-    forall (ez: hasTy Gamma t)
-      (e: hasTy (t :: Gamma) t)
-      (n: hasTy Gamma natTy),
-      hasTy Gamma t.
+    forall (ez: expr Gamma t)
+      (e: expr (t :: Gamma) t)
+      (n: expr Gamma natTy),
+      expr Gamma t.
 Arguments zero {Gamma}.
 
-Definition subst_var Gamma t' (e': hasTy Gamma t') t (v: variable (Gamma ++ [t']) t) : hasTy Gamma t.
+Definition subst_var Gamma t' (e': expr Gamma t') t (v: variable (Gamma ++ [t']) t) : expr Gamma t.
 Admitted.
 
-Definition subst Gamma t' (e': hasTy Gamma t') t (e: hasTy (Gamma ++ [t']) t) : hasTy Gamma t.
+Definition subst Gamma t' (e': expr Gamma t') t (e: expr (Gamma ++ [t']) t) : expr Gamma t.
   intros.
   remember (Gamma ++ [t']).
   induction e; subst.
   + eapply subst_var; eassumption.
 Admitted.
 
-Inductive val Gamma : forall t, hasTy Gamma t -> Prop :=
+Inductive val Gamma : forall t, expr Gamma t -> Prop :=
 | val_z : val zero
-| val_s : forall (e : hasTy Gamma natTy), val e -> val (succ e)
-| val_abs : forall t1 t2 (e: hasTy (t1 :: Gamma) t2), val (abs e).
+| val_s : forall (e : expr Gamma natTy), val e -> val (succ e)
+| val_abs : forall t1 t2 (e: expr (t1 :: Gamma) t2), val (abs e).
 
-Inductive step : forall t, hasTy [] t -> hasTy [] t -> Prop :=
-| step_s : forall (e e': hasTy [] natTy),
+Inductive step : forall t, expr [] t -> expr [] t -> Prop :=
+| step_s : forall (e e': expr [] natTy),
              step e e' ->
              step (succ e) (succ e')
-| step_ap1 : forall t1 t2 (e1 e1': hasTy [] (arrow t1 t2)) e2,
+| step_ap1 : forall t1 t2 (e1 e1': expr [] (arrow t1 t2)) e2,
                step e1 e1' ->
                step (app e1 e2) (app e1' e2)
-| step_ap2 : forall t1 t2 (e1: hasTy [] (arrow t1 t2)) e2 e2',
+| step_ap2 : forall t1 t2 (e1: expr [] (arrow t1 t2)) e2 e2',
                val e1 ->
                step e2 e2' ->
                step (app e1 e2) (app e1 e2')
-| step_apE : forall t1 t2 (e2: hasTy [] t1) (e: hasTy [t1] t2),
+| step_apE : forall t1 t2 (e2: expr [] t1) (e: expr [t1] t2),
                val e2 ->
                step (app (abs e) e2) (subst e2 e)
-| step_iter1 : forall t (ez: hasTy [] t) e n n',
+| step_iter1 : forall t (ez: expr [] t) e n n',
                  step n n' ->
                  step (iter ez e n) (iter ez e n')
-| step_iter2 : forall t (ez: hasTy [] t) e,
+| step_iter2 : forall t (ez: expr [] t) e,
                  step (iter ez e zero) ez
-| step_iter3 : forall t (ez: hasTy [] t) e n,
+| step_iter3 : forall t (ez: expr [] t) e n,
                  step (iter ez e (succ n)) (subst (iter ez e n) e).
