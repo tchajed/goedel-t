1b6a627c02e88bb8027e098d3a416470d7db5cc6
Tej Chajed <tchajed@mit.edu>
2016-07-03 16:59:39 -0600

    Reflect System T terms into Gallina

--- system-t.v
+++ system-t.v
@@ -756,3 +756,48 @@ Proof.
   intros.
   functional induction (eval' e); eauto.
 Qed.
+
+(* Reflecting well-typed terms into Gallina terms *)
+
+Fixpoint type_denote (t: type) : Type :=
+  match t with
+  | natTy => nat
+  | arrow t1 t2 => type_denote t1 -> type_denote t2
+  end.
+
+Fixpoint ctx_denote (Gamma: mapping) : Type :=
+  match Gamma with
+  | nil => unit
+  | t :: Gamma' => type_denote t * ctx_denote Gamma'
+  end.
+
+Fixpoint env_get Gamma (env: ctx_denote Gamma)
+         t (v: variable Gamma t) : type_denote t.
+  destruct v.
+  - exact (fst env).
+  - exact (env_get _ (snd env) _ v).
+Defined.
+
+Fixpoint expr_denote Gamma (t: type) (e: expr Gamma t) :
+  ctx_denote Gamma -> type_denote t.
+  intro env.
+  destruct e; simpl; intros.
+  - exact (env_get env v).
+  - exact 0.
+  - exact (S (expr_denote _ _ e env)).
+  - pose proof (expr_denote _ _ e) as f; simpl in *.
+    exact (f (X, env)).
+  - pose proof (expr_denote _ _ e1) as f; simpl in *.
+    pose proof (expr_denote _ _ e2) as x.
+    exact (f env (x env)).
+  - pose proof (expr_denote _ _ e3 env) as n; simpl in n.
+    induction n.
+    exact (expr_denote _ _ e1 env).
+    pose proof (expr_denote _ _ e2) as iter_f; simpl in *.
+    exact (iter_f (IHn, env)).
+Defined.
+
+Theorem expr_denote_ok : forall t (e: expr [] t),
+    expr_denote e = expr_denote (eval e).
+Proof.
+Abort.
